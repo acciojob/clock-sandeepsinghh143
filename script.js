3a795501fe6632a3d02912625e505af97325ecb9
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2024-03-13 21:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var today = new Date();
 var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+', '+time;
-time.innerText=dateTime;
+timer.innerText=dateTime;
 	console.log(dateTime);
 }
 displayTime();
