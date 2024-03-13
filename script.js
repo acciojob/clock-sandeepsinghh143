5e3732e2aaf708f683ef8c024e0dac41b5071009
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2024-03-13 21:21:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //your JS code here. If required.
 let time=document.getElementById("time");
-setInterval(()=>{
+const displayTime = () => {
 var today = new Date();
 var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+', '+time;
 time.innerText=dateTime;
 	console.log(dateTime);
-},1000);
+}
+displayTime();
+setInterval(displayTime,1000);
